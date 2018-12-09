00939ac9a8af76c3cbd063349adc6ca0f96ac344
WizardZhT <956234606@qq.com>
2018-12-09 22:20:01 +0800

    2

---
+++ imagenet/gpumeasure.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+import argparse
+import operator
+import random
+import re
+
+def gpumeasure(args):
+    txtdir = 'measurepipeline/'
+    pattern = re.compile(r'(?<=gpu_speed\s*:\[)\d+\.?\d*')
+    
+    file_list = os.listdir(txtdir)
+    length = len(file_list)
+
+    outfile = os.path.join(txtdir, 'gpumeasure.txt')
+    out = open(outfile, 'a')
+    for i, file in enumerate(file_list):
+        lines = open(os.path.join(txtdir, file), 'r')
+        speedsum = 0
+        for i, line in enumerate(lines):
+            speed = float(pattern.findall(line))
+            if i > 50 and i < 151:
+                speedsum += speed
+        speedave = speedsum / 100
+        out.writelines(str(speedave), file_list)
+        
+
+if __name__ == '__main__':
+    # args = parser.parse_args()
+    gpumeasure()
