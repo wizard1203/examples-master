90b371ca73ae312a8aa93537abf2aaec65d7ad02
WizardZhT <956234606@qq.com>
2018-12-16 12:35:58 +0800

    22

---
+++ imagenet/measuregpu.sh
@@ -0,0 +1,3 @@
+#!~/miniconda3/envs/py36/bin python
+#!/bin/bash
+python main.py -a alexnet --customize --measure  alexme1 --gpu 1 --lr 0.05 --weight-decay 0.00001 --epochs 95 --kind 000 /media/disk2/data2/imagenet/imagenet_hdf5/
